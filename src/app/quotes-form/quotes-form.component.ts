1ccc9f09b7f078263e4e6dbba822d2f078bf2976
AugustineOchieng <gusochieng@gmail.com>
2019-03-22 15:55:31 +0300

    began value input logic

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.scss']
 })
 export class QuotesFormComponent implements OnInit {
-
+  @Input() value: any;
   constructor() { }
 
   ngOnInit() {

8cd5f9d02406d7d5f0e4878890472d132bbbad78
AugustineOchieng <gusochieng@gmail.com>
2019-03-25 08:35:36 +0300

    add app quotes content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,19 @@ import { Quotes } from './quotes';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "Quotes"
-  quotes = [
-    new Quotes("Gus","William Shakespeare","the future is but the obsolete in reverse","an inspiring quote on the malleability of time"),
-    new Quotes("Gus","Charles de Gaulle","the beauty is in the walking we're betrayed by destinations","an inspiring quote on the malleability of time"),
-    new Quotes("Gus","Shirley Britt","we don't know the fututre but the end is always near","an inspiring quote on the malleability of time"),
-  ]  
+  // title = "Quotes"
+  // quotes = [
+  //   new Quotes("Gus","William Shakespeare","the future is but the obsolete in reverse","an inspiring quote on the malleability of time"),
+  //   new Quotes("Gus","Charles de Gaulle","the beauty is in the walking we're betrayed by destinations","an inspiring quote on the malleability of time"),
+  //   new Quotes("Gus","Shirley Britt","we don't know the fututre but the end is always near","an inspiring quote on the malleability of time"),
+  // ]  
   // toogleDetails(index) {
   //   this.quotes[index].showDetails = !this.quotes[index].showDetails;
   // }
-  addNewQuote(quote) {
-    let quoteLength = this.quotes.length;
-    quote.id = quoteLength + 1;
-    this.quotes.push(quote)
-  }
+  // addNewQuote(quote) {
+  //   let quoteLength = this.quotes.length;
+  //   quote.id = quoteLength + 1;
+  //   this.quotes.push(quote)
+  // }
     }
    
